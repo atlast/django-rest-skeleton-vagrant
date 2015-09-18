ad7d3f550b9ac86c12295e421752ca8bb5054b4b
Chris London <chris.london@goparakeet.com>
2015-09-18 06:33:18 -0600

    skeleton vagrant python django all set up.

--- Vagrantfile
+++ Vagrantfile
@@ -3,8 +3,10 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     config.vm.box = "ubuntu/vivid64"
 
+    config.vm.network "forwarded_port", guest: 8000, host: 8000, auto_correct: true
+
     config.vm.provision :puppet do |puppet|
         puppet.module_path = "modules"
-        puppet.options = "--verbose --debug"
+        # puppet.options = "--verbose --debug"
     end
 end
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,73 +1,48 @@
 node default {
-    class { "aptupdate": stage => "aptupdate" }
-    class { "users": stage => "pre" }
-    class { "python": stage => "py" }
-    class { "application": }
+    class { 'aptupdate': stage => 'aptupdate' }
+    class { 'python': stage => 'py' }
+    class { 'application': }
+    class { 'nginx::override': }
+    class { 'nginx::service-override': }
 }
 
-stage { "aptupdate": before => Stage["pre"] }
+stage { 'aptupdate': before => Stage['py'] }
 class aptupdate {
     exec { 'apt-get update':
         command => '/usr/bin/apt-get update'
     }
 }
 
-stage { "pre": before => Stage["py"] }
-class users {
-    package { 
-        'build-essential': ensure => latest;
-        'libshadow': ensure => latest, provider => 'gem', require => Package["build-essential"];
-    }
-    
-    group { "app":
-        ensure => present,
-        gid => 2000 
-    }
-
-    user { "django":
-        ensure => present,
-        gid => "app",
-        groups => ["adm", "root"],
-        managehome => true,
-        password => '$6$QgrFgGzO$jrmISp8T9vIcKq.DumvXVJcAvcEth7lmLY1EcG.ljkbc3KpU3NEwH8TkQzs4rmUJvsBw5zcUNpWGU4.X84AwB/',
-        shell => "/bin/bash",
-        require => [Group["app"], Package["libshadow"]]
-    }
-
-    file { "/var/www":
-        ensure => "directory",
-        owner  => "vagrant",
-        group  => "app",
-        mode   => 755,
-        require => Group["app"]
-    }
-
-    file { "/var/www/apps":
-        ensure => "directory",
-        owner  => "django",
-        group  => "app",
-        mode   => 755,
-        require => [ Group["app"], User["django"] ] 
+stage { 'py': before => Stage['main'] }
+class python {
+    package {
+        'python-dev': ensure => latest;
+        'python': ensure => latest;
+        'python-pip': ensure => installed;
+        'python-virtualenv': ensure => installed;
+        'virtualenvwrapper': ensure => installed;
     }
 }
 
-stage { "py": before => Stage["main"] }
-class python {
-    package {
-        "python-dev": ensure => "2.7.3-0ubuntu2";
-        "python": ensure => "2.7.3-0ubuntu2";
-        "python-setuptools": ensure => installed;
-        "python-virtualenv": ensure => installed;
-        "virtualenvwrapper": ensure => installed;
+stage { 'nginx::override': before => Stage['main'] }
+class nginx::override inherits nginx::config {
+    File['/etc/nginx/nginx.conf'] {
+        content => template('/vagrant/templates/conf.d/nginx.conf.erb'),
+    }
+}
+
+stage { 'nginx::service-override': before => Stage['main'] }
+class nginx::service-override inherits nginx::service {
+    Service["nginx"] {
+        provider => systemd
     }
 }
 
 class application {
     package {
-        'git-core': ensure => installed;
-        'mercurial': ensure => installed;
-        'uwsgi': ensure => installed;
-        'uwsgi-plugin-python': ensure => installed, require => Package["uwsgi"];
+        'screen': ensure => installed;
+        # 'uwsgi': ensure => installed;
+        # 'uwsgi-plugin-python': ensure => installed, require => Package['uwsgi'];
         # 'libmysqlclient-dev': ensure => installed;
     }
     class { 'nginx': }
@@ -75,10 +50,38 @@ class application {
     #     config_hash => { 'root_password' => '' }
     # }
 
-    sudoers::user { django:
-      ensure => present,
-      nopasswd => true,
-      commands => "ALL",
-      require => [User["django"]]
+    exec { 'create virtual environment':
+        command => '/usr/bin/virtualenv -p /usr/bin/python3 /home/vagrant/.virtualenvs/app',
+        creates => '/home/vagrant/.virtualenvs/app',
+        user => 'vagrant',
+        require => Package['virtualenvwrapper']
+    }
+
+    exec { 'install pip dependencies':
+        command => '/home/vagrant/.virtualenvs/app/bin/pip install -r /vagrant/site/requirements/development.txt',
+        user => 'vagrant',
+        require => Exec['create virtual environment']
+    }
+
+    file { '/etc/systemd/system/django-app.service':
+        ensure => file,
+        content => template('/vagrant/templates/service/django-app.service')
+    }
+
+    file { '/lib/systemd/system/django-app.service':
+        ensure => file,
+        content => template('/vagrant/templates/service/django-app.service')
+    }
+
+    service { 'django-app.service':
+        provider => systemd,
+        ensure => running,
+        enable => true,
+        require => [Exec['install pip dependencies'], File['/etc/systemd/system/django-app.service'], File['/lib/systemd/system/django-app.service']]
+    }
+
+    file { '/etc/nginx/sites-enabled/app':
+        ensure => 'link',
+        target => '/vagrant/templates/conf.d/app.conf.erb',
     }
 }
